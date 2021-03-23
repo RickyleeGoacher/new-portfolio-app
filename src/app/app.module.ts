36c8cb945c985e122af27f4306a4fdda2b91d0cb
Ricky Goacher <rickyleegoacher@hotmail.co.uk>
2021-03-23 22:32:49 +0000

    Components imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,21 @@ import { BannerSliderComponent } from './components/banner-slider/banner-slider.
 
 import { SwiperModule } from 'swiper/angular';
 import { SkillsSliderComponent } from './components/skills-slider/skills-slider.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     BannerSliderComponent,
-    SkillsSliderComponent
+    SkillsSliderComponent,
+    ProjectsComponent,
+    FooterComponent,
+    ContactFormComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
