c47b5ad0af1d214464caf284772ec3182d4a314e
Ricky Goacher <rickyleegoacher@hotmail.co.uk>
2021-03-23 22:37:10 +0000

    Markup, styles and scroll javascript added

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -10,6 +10,16 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  	let prevOffset = window.pageYOffset;
+  	window.addEventListener('scroll', function() {
+  		const navElement = document.querySelector('#nav') as HTMLElement;
+  		if(window.pageYOffset > prevOffset) {
+  			navElement.classList.add('hide');
+  		} else {
+  			navElement.classList.remove('hide');
+  		}
+  		prevOffset = window.pageYOffset;
+  	});
   }
 
 }
