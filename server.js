ad24aa74136f34638a7823d511a8d5b219323eb2
Ricky Goacher <rickyleegoacher@hotmail.co.uk>
2021-03-26 18:34:15 +0000

    heroku upload setup

--- server.js
+++ server.js
@@ -1,20 +1,15 @@
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
 const express = require('express');
+const path = require('path');
+
 const app = express();
-app.use(requireHTTPS);
 
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/new-portfolio-app'));
 
-app.use(express.static('./dist/new-portfolio-app'));
-
-app.get("/*", function(req, res) {
-  res.sendFile('index.html', {root: 'dist/<name-on-package.json>/'}
-);
-
-  app.listen(process.env.PORT || 8080);
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/new-portfolio-app/index.html'));
 });
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
