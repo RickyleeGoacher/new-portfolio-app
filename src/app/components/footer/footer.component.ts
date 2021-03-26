265f907000a43a141adb34bc6df6a72cad883fb9
Ricky Goacher <rickyleegoacher@hotmail.co.uk>
2021-03-26 17:41:17 +0000

    Add back to top button, styles, markup + js

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -10,6 +10,20 @@ export class FooterComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  	window.addEventListener('scroll', function() {
+  		const navElement = document.querySelector('#top-button') as HTMLElement;
+  		console.log(window.pageYOffset)
+  		if(window.pageYOffset < 550) {
+  			navElement.classList.add('hide');
+  		} else {
+  			navElement.classList.remove('hide');
+  		}
+  	});
   }
 
-}
+  pageTop() {
+  	window.scrollTo(0, 0);
+  }
+
+  }
+
