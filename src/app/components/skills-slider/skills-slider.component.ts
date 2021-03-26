c222e39e8ed3fe4f04b6096294d115d87711e057
Ricky Goacher <rickyleegoacher@hotmail.co.uk>
2021-03-26 14:26:08 +0000

    Skills slider styles, animations + markup added

--- src/app/components/skills-slider/skills-slider.component.ts
+++ src/app/components/skills-slider/skills-slider.component.ts
@@ -1,28 +1,49 @@
 import { Component, OnInit } from '@angular/core';
-import SwiperCore, {
-  Navigation,
-  A11y,
-} from 'swiper/core';
-
-SwiperCore.use([Navigation, A11y]);
+import { SwiperOptions } from 'swiper';
+import Swiper, { Autoplay} from 'swiper';
+Swiper.use([Autoplay]);
 
 @Component({
   selector: 'app-skills-slider',
   templateUrl: './skills-slider.component.html',
   styleUrls: ['./skills-slider.component.less']
 })
+
 export class SkillsSliderComponent implements OnInit {
 
-  constructor() { }
-
-  onSwiper(swiper: any) {
-    console.log(swiper);
-  }
-  onSlideChange() {
-    console.log('slide change');
-  }
+  constructor() { } 
 
   ngOnInit(): void {
   }
+
+  config: SwiperOptions = {
+    pagination: { el: '.swiper-pagination', clickable: true },
+    autoHeight: true,
+    allowTouchMove: true,
+    breakpoints: {
+      1024: {
+        slidesPerView: 5
+      },
+      500: {
+        slidesPerView: 3
+      },
+      400: {
+        slidesPerView: 2
+      },
+      300: {
+        slidesPerView: 2
+      }
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: true
+    },
+    loop: true,
+    slideToClickedSlide: true 
+  };
 
 }
